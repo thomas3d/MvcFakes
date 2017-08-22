72264943a1359223311064bd6e2d1b598dbaf72d
thomas3d <thomas@etjam.dk>
2017-08-22 16:31:19 +0200

    Update FakeHttpContext.cs

--- FakeHttpContext.cs
+++ FakeHttpContext.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
 using System.Security.Principal;
 using System.Web;
+using System.Web.Instrumentation;
 using System.Web.SessionState;
 
 namespace FlickTrap.Web.Specs.MvcFakes
 {
-
 
     public class FakeHttpContext : HttpContextBase
     {
@@ -14,14 +19,19 @@ namespace FlickTrap.Web.Specs.MvcFakes
         private readonly NameValueCollection _queryStringParams;
         private readonly HttpCookieCollection _cookies;
         private readonly SessionStateItemCollection _sessionItems;
+        private readonly TextWriter _writer;
+        private readonly Dictionary<object, object> _dic;
 
-        public FakeHttpContext(FakePrincipal principal, NameValueCollection formParams, NameValueCollection queryStringParams, HttpCookieCollection cookies, SessionStateItemCollection sessionItems )
+        public FakeHttpContext(FakePrincipal principal, NameValueCollection formParams, NameValueCollection queryStringParams, HttpCookieCollection cookies, SessionStateItemCollection sessionItems, TextWriter writer)
         {
             _principal = principal;
             _formParams = formParams;
             _queryStringParams = queryStringParams;
             _cookies = cookies;
             _sessionItems = sessionItems;
+            _writer = writer;
+            _dic = new Dictionary<object, object>();
+            ApplicationInstance = HttpContext.Current.ApplicationInstance;
         }
 
         public override HttpRequestBase Request
@@ -29,6 +39,14 @@ namespace FlickTrap.Web.Specs.MvcFakes
             get
             {
                 return new FakeHttpRequest(_formParams, _queryStringParams, _cookies);
+            }
+        }
+
+        public override HttpResponseBase Response
+        {
+            get
+            {
+                return new FakeHttpResponse(_writer, _cookies);
             }
         }
 
@@ -50,6 +68,41 @@ namespace FlickTrap.Web.Specs.MvcFakes
             {
                 return new FakeHttpSessionState(_sessionItems);
             }
+        }
+
+        public override IDictionary Items
+        {
+            get
+            {
+                return _dic;
+            }
+        }
+
+        public override PageInstrumentationService PageInstrumentation
+        {
+            get { return new PageInstrumentationService(); }
+        }
+
+        public override object GetService(Type serviceType)
+        {
+            return null;
+        }
+
+        public override object GetGlobalResourceObject(string classKey, string resourceKey, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override HttpServerUtilityBase Server
+        {
+            get { return new HttpServerUtilityWrapper(HttpContext.Current.Server ); }
+        }
+
+        public override HttpApplication ApplicationInstance { get; set; }
+
+        public override HttpApplicationStateBase Application
+        {
+            get { return new HttpApplicationStateWrapper(HttpContext.Current.Application); }
         }
 
     }
